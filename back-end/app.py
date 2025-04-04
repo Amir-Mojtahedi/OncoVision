b7072f74fca44b25ddc62c91e83f36e8efd44fd1
Amir Mojtahedi <amirrezamojtahedi2@gmail.com>
2025-04-04 16:06:44 -0400

    Add svm model to the code

--- back-end/app.py
+++ back-end/app.py
@@ -4,6 +4,7 @@ import numpy as np
 from ai.logisticModel import logisticRegrssionModel
 from ai.decisionTree import decisionTreeModel
 from ai.randomforestmodel import randomForestModel
+from ai.svm import svmModel
 from ai.utils.aiUtils import scaler, top_5
 
 app = Flask(__name__)
@@ -26,7 +27,19 @@ def ai_model():
         input_scaled = scaler.transform(input_array)
 
         # Predict using the model
-        prediction = logisticRegrssionModel.predict(input_scaled)[0]
+        prediction = None
+        match data['model']:
+            case 'logistic':
+                prediction = logisticRegrssionModel.predict(input_scaled)[0]
+            case 'randomforest':
+                prediction = randomForestModel.predict(input_scaled)[0]
+            case 'decisiontree':
+                prediction = decisionTreeModel.predict(input_scaled)[0]
+            case 'svm':
+                prediction = svmModel.predict(input_scaled)[0]
+            case _:
+                return jsonify({"error": "Invalid model name"}), 400
+
         tumor = "Malignant" if prediction == 1 else "Benign"
         
         # Return the prediction
